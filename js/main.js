412049c31818885cbab0056b0b2bf8cdde056689
Pablo Andrés Valdivia <pandresvaldiviacuzcano@gmail.com>
2021-11-01 14:58:42 -0500

    File paths fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import UI from './classes/ui.js';
 
-const ui = new UI('../assets/json/data.json', '../assets/images/');
+const ui = new UI('./assets/json/data.json', './assets/images/');
 
 document.addEventListener('DOMContentLoaded', () => {
 	ui.createSection();
